c01744046fa7826748effd8d2b19234021c8d483
Jeremias Brussino <98906318+Assietta@users.noreply.github.com>
2023-05-03 17:35:39 -0300

    Update createPokemon.js

--- api/src/controllers/modulos/createPokemon.js
+++ api/src/controllers/modulos/createPokemon.js
@@ -1,8 +1,9 @@
 const { pokemon, type } = require('../../db');
+const { v4: uuidv4 } = require('uuid');
 
 const createPokemon = async (id, name, vida, ataque, defensa, velocidad, altura, peso, type) =>
   await pokemon.create({
-    id,
+    id: uuidv4(),
     name,
     vida,
     ataque,
