1ebb4cd9d9535799700a8109f9acedef9041e616
Jeremias Brussino <98906318+Assietta@users.noreply.github.com>
2023-05-06 18:16:11 -0300

    filtraaaa

--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,61 +1,75 @@
-import { GET_POKEMONS, GET_TYPES, GET_ID, GET_NAME, POST_POKEMON, FILTER_BY_TYPE, FILTER_BY_CREATED } from "./actions";
+import {
+  GET_POKEMONS,
+  GET_TYPES,
+  GET_ID,
+  GET_NAME,
+  POST_POKEMON,
+  FILTER_BY_TYPE,
+  FILTER_BY_CREATED,
+} from "./actions";
 
 const initialState = {
-    pokemons: [],
-    types: [],
-    pokemonsID: {},
-    pokemonsNAME: {},
-  }
-  
-  const rootReducer = (state = initialState, action) => {
-      switch (action.type) {
-        case GET_POKEMONS:
-        return {...state,
-           pokemons: action.payload,
-          };
-        case GET_TYPES:
-        return {...state,
-            types: action.payload,
-              };
-        case GET_ID:
-        return {...state,
-            pokemonsID: action.payload,
-              };
-        case GET_NAME:
-        return {...state,
-            pokemons: action.payload,
-              };
-        case POST_POKEMON:
-          return {...state,
-            pokemons: action.payload,
-              };
-              
-              case FILTER_BY_TYPE:
-                const selectedTypes = action.payload;
-                const filteredPokemons = state.pokemons.filter(pokemon => {
-                  // Si no hay tipos seleccionados, no se filtra nada
-                  if (selectedTypes.length === 0) {
-                    return true;
-                  }
-                  // Si hay al menos un tipo seleccionado, se filtra según esos tipos
-                  return selectedTypes.every(type => pokemon.tipos.includes(type));
-                });
-                return {
-                  ...state,
-                  pokemons: filteredPokemons
-                }
+  pokemons: [],
+  types: [],
+  pokemonsID: {},
+  pokemonsNAME: {},
+};
 
-              
-              
-        case FILTER_BY_CREATED:
-          const pokemonsByCreated = state.pokemons.filter(pokemon => pokemon.isDB === action.payload)
-          return {
-            ...state,
-            pokemons: pokemonsByCreated
-          }
-        default:
-              return { ...state };
-      }
+function filterPokemons(state, action) {
+  const selectedTypes = action.payload;
+  const filteredPokemons = state.allPokemons.filter((pokemon) => {
+    if (selectedTypes.length === 0) {
+      return true;
+    }
+    return selectedTypes.every((type) => pokemon.tipos.includes(type));
+  });
+  return {
+    ...state,
+    pokemons: filteredPokemons,
   };
-  
-  export default rootReducer;
+}
+
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_POKEMONS:
+      return {
+        ...state,
+        pokemons: action.payload,
+        allPokemons: action.payload,
+      };
+    case GET_TYPES:
+      return {
+        ...state,
+        types: action.payload,
+      };
+    case GET_ID:
+      return {
+        ...state,
+        pokemonsID: action.payload,
+      };
+    case GET_NAME:
+      return {
+        ...state,
+        pokemons: action.payload,
+      };
+    case POST_POKEMON:
+      return {
+        ...state,
+        pokemons: action.payload,
+      };
+    case FILTER_BY_TYPE:
+      return filterPokemons(state, action);
+    case FILTER_BY_CREATED:
+      const pokemonsByCreated = state.allPokemons.filter(
+        (pokemon) => pokemon.isDB === action.payload
+      );
+      return {
+        ...state,
+        pokemons: pokemonsByCreated,
+      };
+    default:
+      return { ...state };
+  }
+};
+
+export default rootReducer;
